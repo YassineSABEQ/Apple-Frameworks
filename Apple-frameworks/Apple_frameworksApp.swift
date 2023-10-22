079df471fbb3e82ac6346e5e134daef48820b7a7
Yassine SABEQ <yassine.sabeq@babelgroup.com>
2023-10-22 18:41:09 +0100

    Initial Commit

---
+++ Apple-frameworks/Apple_frameworksApp.swift
@@ -0,0 +1,17 @@
+//
+//  Apple_frameworksApp.swift
+//  Apple-frameworks
+//
+//  Created by Yassine Sabeq on 22/10/2023.
+//
+
+import SwiftUI
+
+@main
+struct Apple_frameworksApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
