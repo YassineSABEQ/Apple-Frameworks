2161f5ad8af334ad0e09ace95804df7f3870b253
Yassine SABEQ <yassine.sabeq@babelgroup.com>
2023-10-23 16:19:05 +0100

    frameworks grid implemented

--- Apple-frameworks/ContentView.swift
+++ Apple-frameworks/ContentView.swift
@@ -8,19 +8,48 @@
 import SwiftUI
 
 struct ContentView: View {
+    
+    let columns: [GridItem] = [GridItem(.flexible()),
+                               GridItem(.flexible()),
+                               GridItem(.flexible())]
+    
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundColor(.accentColor)
-            Text("Hello, world!")
-        }
-        .padding()
+            NavigationView {
+                ScrollView(showsIndicators: false) {
+                    LazyVGrid(columns: columns) {
+                        ForEach(MockData.frameworks) { framework in
+                            AppFrameworkView(framework: framework)
+                        }
+                    }
+                }
+                .navigationTitle("🍎 Frameworks")
+            }
+            .preferredColorScheme(.dark)
     }
 }
 
 struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
         ContentView()
+    }
+}
+
+struct AppFrameworkView: View {
+    let framework: Framework
+    
+    var body: some View {
+        VStack {
+            Image(framework.imageName)
+                .resizable()
+                .aspectRatio(contentMode: .fit)
+                .frame(width: 80, height: 80)
+            Text(framework.name)
+                .font(.title2)
+                .fontWeight(.semibold)
+                .scaledToFit()
+                .minimumScaleFactor(0.6)
+                .foregroundColor(.white)
+        }
+        .padding()
     }
 }
